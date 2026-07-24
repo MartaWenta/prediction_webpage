67518facba2a16f2fcdeceb5efa0c4d2ceb2318f
Adrien Damseaux <adrien.damseaux@kit.edu>
2026-07-24 11:50:25 +0200

    Fix previous commit: add references.js file

---
+++ references.js
@@ -0,0 +1,19 @@
+window.BASIS_BIBLIO = {
+  'schmolke 2010': 'Schmolke, A., Thorbek, P., DeAngelis, D. L., & Grimm, V. (2010). Ecological models supporting environmental decision making: A strategy for the future. Trends in Ecology and Evolution, 25(8), 479-486. https://doi.org/10.1016/j.tree.2010.05.001',
+  'getz 2018': 'Getz, W. M., Marshall, C. R., Carlson, C. J., Giuggioli, L., Ryan, S. J., Romanach, S. S., Boettiger, C., Chamberlain, S. D., Larsen, L., D\'Odorico, P., & O\'Sullivan, D. (2018). Making ecological models adequate. Ecology Letters, 21(2), 153-166. https://doi.org/10.1111/ele.12893',
+  'sun 2015': 'Sun, N.-Z., & Sun, A. (2015). Model Calibration and Parameter Estimation: For Environmental and Water Resource Systems. Springer.',
+  'benaroya 2005': 'Benaroya, H., & Han, S. M. (2005). Probability Models in Engineering and Science. Taylor & Francis.',
+  'committee 2012': 'Committee on Mathematical Foundations of Verification, Validation, and Uncertainty Quantification, & Board on Mathematical Sciences and Their Applications, Division on Engineering and Physical Sciences, National Research Council. (2012). Assessing the Reliability of Complex Models: Mathematical and Statistical Foundations of Verification, Validation and Uncertainty Quantification. The National Academic Press. https://doi.org/10.17226/13395',
+  'dormann 2018': 'Dormann, C. F., Calabrese, J. M., Guillera-Arroita, G., Matechou, E., Bahn, V., Barton, K., ... & Hartig, F. (2018). Model averaging in ecology: A review of Bayesian, information-theoretic, and tactical approaches for predictive inference. Ecological Monographs, 88(4), 485-504. https://doi.org/10.1002/ecm.1309',
+  'roberts 2017': 'Roberts, D. R., Bahn, V., Ciuti, S., Boyce, M. S., Elith, J., Guillera-Arroita, G., ... & Dormann, C. F. (2017). Cross-validation strategies for data with temporal, spatial, hierarchical, or phylogenetic structure. Ecography, 40(8), 913-929. https://doi.org/10.1111/ecog.02881',
+  'dormann 2026': 'Dormann, C. F., Kaber, Y., & Hartig, F. (2026). Full prediction uncertainty quantification: A plea from science and decision making. In Advances in Ecological Research (Vol. 74, pp. 191-212). Elsevier. https://doi.org/10.1016/bs.aecr.2026.02.003',
+  'matheson 1976': 'Matheson, J. E., & Winkler, R. L. (1976). Scoring rules for continuous probability distributions. Management Science, 22(10), 1087-1096. https://doi.org/10.1287/mnsc.22.10.1087',
+  'taylor 1997': 'Taylor, J. R. (1997). An Introduction to Error Analysis: The Study of Uncertainties in Physical Measurements (2nd ed.). University Science Books.',
+  'wesselkamp 2025': 'Wesselkamp, M., Albrecht, J., Pinnington, E., Castillo, W. J., Pappenberger, F., & Dormann, C. F. (2025). The ecological forecast limit revisited: Potential, absolute and relative system predictability. Methods in Ecology and Evolution, 16(7), 1521-1541. https://doi.org/10.1111/2041-210X.70049',
+  'wolter 2007': 'Wolter, K. M. (2007). Introduction to Variance Estimation (2nd ed.). Springer.',
+  'marjoram 2003': 'Marjoram, P., Molitor, J., Plagnol, V., & Tavare, S. (2003). Markov chain Monte Carlo without likelihoods. Proceedings of the National Academy of Sciences, 100(26), 15324-15328. https://doi.org/10.1073/pnas.0306899100',
+  'luo 2009': 'Luo et al. (2009). [please add full bibliographic details].',
+  'oberpriller 2021': 'Oberpriller et al. (2021). [please add full bibliographic details].'
+};
+
+window.MODEL_INVERSION_BIBLIO = window.BASIS_BIBLIO;
